ded383fe8ed0139d4217366575cb747e5c62a69b
Carl <carlbrainegebe@gmail.com>
2021-06-19 02:45:53 -0500

    commit tree 4836811812a20dcd29fcd0ec8dace92fdd15e6cc

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [condition, setCondition] = useState(false);
+  //create function that gets called when button element is clicked
+  function handleClick() {
+    return setCondition(!condition)
+  }
+  return <button onClick={()=>{handleClick()}}>{condition? "ON": "OFF"}</button>;
 }
 
 export default Toggle;
